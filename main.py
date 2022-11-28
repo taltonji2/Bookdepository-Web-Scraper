d5a960c689a974827a081d8522d9ed33fe6de24e
tim <timaltonji@protonmail.com>
2022-11-28 15:12:25 -0500

    json book list

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 from bs4 import BeautifulSoup
 import requests
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import re
 
-html = requests.get('https://www.bookdepository.com/')
-soup = BeautifulSoup(html.text, 'lxml')
-books = soup.find('div', class_ ='book-item')
-# print(books)
-for book in books: 
-  book_title = book.find('div', class_ ='item-info').h3.a.text.replace('  ', '')
-  book_author = book.find('div', class_ ='item-info').p.span.a.span.text
-#   book_price = book.find('',).text.replace(' ', '')
-  print(book_title)
 
+def get_data():
+  page = 1
+  books = []
+
+  while page != 5:
+    response = requests.get(
+        f"https://www.bookdepository.com/bestsellers?page={page}")
+    html = response.content
+    soup = BeautifulSoup(html, "lxml")
+
+    for book_item in soup.find_all("div", class_="book-item"):
+        book = {}
+        book["title"] = book_item.find("h3", class_="title").a.get_text(strip=True)
+        book["author"] = book_item.find("p", class_="author").get_text(strip=True)
+        book["format"] = book_item.find("p", class_="format").get_text(strip=True)
+        book["publish"] = book_item.find("p", class_="published").get_text(strip=True)
+        try:
+          price = str(book_item.find("p", class_="price").find("span", class_="sale-price"))[28:33].replace('<',"")
+        except:
+          price = ""
+        book["price"] = price
+        books.append(book)
+
+    page = page + 1
+  
+  print(books)
+
+
+get_data()
